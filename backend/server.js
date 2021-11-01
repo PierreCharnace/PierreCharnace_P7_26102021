35c6658523e7c1a3a9625d7b688953b93610d1da
PierreCharnace <pierre.charnace@gmail.com>
2021-11-01 14:34:41 +0100

    make users control

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,16 @@ const apiRouter = require('./apiRouter').router;
 
 //instantiate server
 const server = express();
-//for body-parser
-server.use(express.urlencoded({extended: true}));
-server.use(express.json());
 
 //configure routes
 server.get('/', function (req, res) {
   res.setHeader('Content-Type', 'text/html');
   res.status(200).send('<h1>Bonjour sur mon serveur<h1>');
 });
+
+//for body-parser
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
 
 server.use('/api/', apiRouter);
 
