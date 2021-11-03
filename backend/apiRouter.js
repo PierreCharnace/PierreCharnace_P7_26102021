590092a2bf35f97a5e1d8fcd853a0948e3e20cd3
PierreCharnace <pierre.charnace@gmail.com>
2021-11-03 16:14:46 +0100

    make getuserPorfile and updateUserProfile in usersCtrl.js

--- backend/apiRouter.js
+++ backend/apiRouter.js
@@ -10,7 +10,8 @@ exports.router = (function() {
     //Users routes
     apiRouter.route('/users/register/').post(usersCtrl.register);
     apiRouter.route('/users/login/').post(usersCtrl.login);
-
+    apiRouter.route('/users/userProfile/').get(usersCtrl.getUserProfile);
+    apiRouter.route('/users/userProfile/').put(usersCtrl.updateUserProfile);
     return apiRouter;
 })();
 
--- backend/routes/usersCtrl.js
+++ backend/routes/usersCtrl.js
@@ -41,52 +41,52 @@ module.exports = {
 
         asyncLib.waterfall([
             function (done) {
-                done(null);
-            }
-        ], function(err) {
-            if(!err) {
-                return res.status(200).json({ 'msg': 'ok'});
-            }   else {
-                return rmSync.status(404).json({ 'error': 'erreur'});
-            }
-        })
-
-        models.User.findOne({
-            attributes: ['email'],
-            where: { email: email }
-        })
-        .then(function(userFound) {
-            if (!userFound) {
-
-                bcrypt.hash(password, 5, function( err, bcryptedPassword ) {
-                    const newUser = models.User.create({
-                        email : email,
-                        lastName : lastName,
-                        firstName : firstName,                       
-                        birthday : birthday,
-                        password : bcryptedPassword,
-                        profilePictures : profilePictures,
-                        isEnable : 0,
-                        isAdmin : 0
-                    })
-               
+                models.User.findOne({
+                    attributes: ['email'],
+                    where: { email: email }
+                })
+                .then(function(userFound) {
+                    done(null, userFound);
+                })
+                .catch(function(err) {
+                    return res.status(500).json({ 'error': 'unable to verify user'});
+                });
+            },
+            function(userFound, done) {
+                if (!userFound) {
+                  bcrypt.hash(password, 5, function( err, bcryptedPassword ) {
+                    done(null, userFound, bcryptedPassword);
+                  });
+                } else {
+                  return res.status(409).json({ 'error': 'user already exist' });
+                }
+            },
+        function(userFound, bcryptedPassword, done) {
+                const newUser = models.User.create({
+                    email : email,
+                    lastName : lastName,
+                    firstName : firstName,                       
+                    birthday : birthday,
+                    password : bcryptedPassword,
+                    profilePictures : profilePictures,
+                    isEnable : 0,
+                    isAdmin : 0
+                })
                 .then(function(newUser) {
-                    return res.status(201).json({
-                        'userId': newUser.id,
-                    })
+                    done(newUser);
                 })
                 .catch(function(err) {
                     return res.status(500).json({ 'error': 'cannot add user'});
                 });
-            })
-
-            } else {
-                return res.status(409).json({ 'error': 'user already exist' });
             }
-
-        })
-        .catch(function(err) {
-            return res.status(500).json({ 'error': 'unable to verify user'})
+        ], function (newUser) {
+            if (newUser) {
+                return res.status(201).json({
+                    'userId': newUser.id
+                });
+            } else {
+                return res.status(500).json({ 'error': 'cannot add user' })
+            }
         });
 
     },
@@ -96,32 +96,110 @@ module.exports = {
         const password = req.body.password;
 
         if (email == null || password == null) {
-            return res.status(400).json({ 'error': 'missing parameters' });
+            return res.status(400).json({ 'error': 'paramètres manquants' });
         }
-        //todo verify mail regex & password length
 
+        asyncLib.waterfall([
+      function(done) {
         models.User.findOne({
-            where: { email }
+          where: { email: email }
         })
         .then(function(userFound) {
-            if (userFound) {
-                bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt) {
-                    if(resBycrypt) {
-                        return res.status(200).json({
-                            'userId': userFound.id,
-                            'token': jwtUtils.generateTokenForUser(userFound)
-                        });
-                    } else {
-                        return res.status(403).json({ 'error': 'invalid password' });
-                    }
-                });
-
-            } else {
-                return res.status(404).json({ 'error': 'user not exist in DB'});
-            }
+          done(null, userFound);
         })
         .catch(function(err) {
-            return res.status(500).json({ 'error': 'unable to verify user' });
+          return res.status(500).json({ 'error': 'unable to verify user' });
         });
+      },
+      function(userFound, done) {
+        if (userFound) {
+          bcrypt.compare(password, userFound.password, function(errBycrypt, resBycrypt) {
+            done(null, userFound, resBycrypt);
+          });
+        } else {
+          return res.status(404).json({ 'error': 'user not exist in DB' });
+        }
+      },
+      function(userFound, resBycrypt, done) {
+        if(resBycrypt) {
+          done(userFound);
+        } else {
+          return res.status(403).json({ 'error': 'invalid password' });
+        }
+      }
+    ], function(userFound) {
+      if (userFound) {
+        return res.status(201).json({
+          'userId': userFound.id,
+          'token': jwtUtils.generateTokenForUser(userFound)
+        });
+      } else {
+        return res.status(500).json({ 'error': 'cannot log on user' });
+      }
+    });
+  },
+  getUserProfile: function(req, res) {
+    // Getting auth header
+    const headerAuth = req.headers['authorization'];
+    const userId = jwtUtils.getUserId(headerAuth);
+
+    if (userId < 0)
+      return res.status(400).json({ 'error': 'wrong token' });
+
+    models.User.findOne({
+      attributes: [ 'id', 'lastName', 'firstName', 'email', 'birthday', 'profilePictures' ],
+      where: { id: userId }
+    }).then(function(user) {
+      if (user) {
+        res.status(201).json(user);
+      } else {
+        res.status(404).json({ 'error': 'user not found' });
+      }
+    }).catch(function(err) {
+      res.status(500).json({ 'error': 'cannot fetch user' });
+    });
+  },
+  updateUserProfile: function(req, res) {
+    // Getting auth header
+    const headerAuth = req.headers['authorization'];
+    const userId = jwtUtils.getUserId(headerAuth);
+
+    // Params
+    const birthday = req.body.birthday;
+    const profilePictures = req.body.profilePictures;
+
+    asyncLib.waterfall([
+      function(done) {
+        models.User.findOne({
+          attributes: [ 'id', 'birthday', 'profilePictures' ],
+          where: { id: userId }
+        }).then(function (userFound) {//return user
+          done(null, userFound); // next function with done
+        })
+        .catch(function(err) {
+          return res.status(500).json({ 'error': 'unable to verify user' });
+        });
+      },
+      function(userFound, done) {
+        if(userFound) {
+          userFound.update({
+            birthday: (birthday ? birthday : userFound.birthday),                           // verify if birthday and profilePictures is valid in req, if ok, 
+            porfilePictures: (profilePictures ? profilePictures : userFound.profilePictures) // substitute or if same thing I let them
+          }).then(function() {
+            done(userFound); // when is update, return userFound, the waterfall is done
+          }).catch(function(err) {
+            res.status(500).json({ 'error': 'cannot update user' });
+        });
+    } else {
+      res.status(404).json({ 'error': 'user not found' });
     }
+  },
+], function(userFound) {// if userFound is valid I return status 200
+  if (userFound) {
+    return res.status(201).json(userFound);
+  } else {
+    return res.status(500).json({ 'error': 'cannot update user profile' });
+  }
+});
+}
 }
