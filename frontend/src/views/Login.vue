46a758fa8ad5c2f53cf374aa19c1a61eabe1258b
PierreCharnace <pierre.charnace@gmail.com>
2022-01-07 12:14:40 +0100

    make pattern for input login and register

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="container-fluid text-center login">
-     <form class="container-fluid align-self-item">
+     <form class="container-fluid align-self-item pt-5">
       <h1 class="">Connexion</h1>
       <p>Vous n'avez pas encore de compte? <router-link class="createaccount" to="/register" >Créer un compte</router-link></p>
      <div class="row mt-1">
-        <input v-model="email" id="email" required type="email" class="col-5" placeholder="Adresse mail">
+        <input pattern="[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z.]{2,15}" v-model="email" id="email" required type="email" class="col-5" placeholder="Adresse mail">
       </div>
       <div class="row mt-1">
-        <input v-model="password" required id="password" type="password" class="col-5" placeholder="mode de passe">
+        <input pattern="[a-z0-9.]{4,8}" title="Le mot de passe doit être compris entre 4 et 8 caractères et contenir au moins 1 chiffre" v-model="password" required id="password" type="password" class="col-5" placeholder="mot de passe">
       </div>
-      <div class="row mt-1" v-if="status == 'error_login'"> Adresse mail et/ou mot de passe invalide</div>
+      <div  class="row mt-1" v-if="status == 'error_login'"> Adresse mail et/ou mot de passe invalide</div>
       <button @click="login()" class="buttonform mt-2 mb-2" >
-        <span v-if="status == 'loading'">Connexion en cours...</span>
-        <span v-else> Connexion</span>
+        <span > Connexion</span>
       </button>
       
     </form>
@@ -52,6 +51,11 @@ export default {
 </script>
 
 <style scoped>
+
+input {
+  min-width: 300px;
+  max-width: 500px;
+}
 
 form div {
   display: flex;
--- frontend/src/views/Register.vue
+++ frontend/src/views/Register.vue
@@ -1,20 +1,20 @@
 <template>
 <div class="container-fluid text-center register">
   
-    <form class="container-fluid">
+    <form class="container-fluid pt-5">
       <h1 class="">Page d'enregistrement</h1>
       <p>Vous avez déjà un compte? <router-link class="createaccount" to="/login" >Connectez-vous</router-link></p>
       <div class="row mt-1">
-        <input v-model="lastName" type="text" id="lastname" required class="col-5 " placeholder="Nom">
+        <input pattern="[a-zA-Z.]{2,30}" title="Le nom doit être compris entre 2 et 30 caractères" v-model="lastName" type="text" id="lastname" required class="col-5 " placeholder="Nom">
       </div>
       <div class="row mt-1">
-        <input v-model="firstName" id="firstname" required type="text" class="col-5" placeholder="Prénom">
+        <input pattern="[a-zA-Z.]{2,20}" title="Le nom doit être compris entre 2 et 20 caractères" v-model="firstName" id="firstname" required type="text" class="col-5" placeholder="Prénom">
       </div>
       <div class="row mt-1">
-        <input v-model="email" id="email" required type="email" class="col-5" placeholder="Adresse mail">
+        <input pattern="[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+.[a-zA-Z.]{2,15}" v-model="email" id="email" required type="email" class="col-5" placeholder="Adresse mail">
       </div>
       <div class="row mt-1">
-        <input v-model="password" required="veuillez" id="password" type="password" class="col-5" placeholder="mot de passe">
+        <input pattern="[a-z0-9]{4,8}" title="Le mot de passe doit être compris entre 4 et 8 caractères minuscules et contenir au moins 1 chiffre" v-model="password" required="veuillez" id="password" type="password" class="col-5" placeholder="mot de passe">
       </div>
       <button @click="createAccount()" class="buttonform mt-2 mb-2" >
        <span v-if="validateFields == false"> Veuillez remplir les champs</span>
@@ -37,8 +37,6 @@ export default {
     }
   },
   computed : {
-
-
     validateFields: function () {
       if (this.lastName !="" && this.firstName !="" && this.email != "" && this.password != "") {
         return true;
@@ -78,6 +76,10 @@ export default {
 
 </style>
 <style scoped>
+input {
+  min-width: 300px;
+  max-width: 500px;
+}
 
 form div {
   display: flex;
